88d5b7b6135d28e8c96a47f040df05c88a490cf9
hnewcom47 <71475246+hnewcom47@users.noreply.github.com>
2021-03-25 17:41:45 -0500

    Created welcome page, profile, update profile and nav bar components

---
+++ src/app/update-user-profile/update-user-profile.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { EditUserService } from '../fetch-api-data.service';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-update-profile',
+  templateUrl: './update-user-profile.component.html',
+  styleUrls: ['./update-user-profile.component.scss']
+})
+
+export class UpdateProfileComponent implements OnInit {
+
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+
+  constructor(
+    public snackBar: MatSnackBar,
+    public fetchApiData: EditUserService,
+    public dialogRef: MatDialogRef<UpdateProfileComponent>
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  editUser(): void {
+    this.fetchApiData.editUser(this.userData).subscribe((response) => {
+      this.dialogRef.close();
+      localStorage.setItem('user', response.Username);
+      this.snackBar.open('Profile updated successfully!', 'OK', {
+        duration: 2000,
+      });
+    }, (response) => {
+      console.log(response);
+      this.snackBar.open(response, 'OK', {
+        duration: 2000,
+      });
+    });
+    setTimeout(function () {
+      window.location.reload();
+    }, 1250);
+  }
+}
